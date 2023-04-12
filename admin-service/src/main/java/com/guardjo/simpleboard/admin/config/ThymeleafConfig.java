1ca705b35ad4b2f31994d616de4dff9f5b7f3f3f
kyeongho <dagh1218@naver.com>
2023-04-12 23:21:50 +0900

    #6 불필요한 import 정리

--- admin-service/src/main/java/com/guardjo/simpleboard/admin/config/ThymeleafConfig.java
+++ admin-service/src/main/java/com/guardjo/simpleboard/admin/config/ThymeleafConfig.java
@@ -1,9 +1,6 @@
 package com.guardjo.simpleboard.admin.config;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
