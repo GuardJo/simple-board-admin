a6f192a61ab4a550b8457a84c98a3013921a43c0
kyeongho <dagh1218@naver.com>
2023-05-13 00:01:45 +0900

    #15 api url 들을 기본을 복수형으로 변경
    - ex) /article -> /articles

--- admin-service/src/main/java/com/guardjo/simpleboard/admin/controller/constant/UrlConstant.java
+++ admin-service/src/main/java/com/guardjo/simpleboard/admin/controller/constant/UrlConstant.java
@@ -1,8 +1,8 @@
 package com.guardjo.simpleboard.admin.controller.constant;
 
 public class UrlConstant {
-    public final static String ARTICLE_MANAGEMENT_URL_PREFIX = "/management/article";
-    public final static String COMMENT_MANAGEMENT_URL_PREFIX = "/management/comment";
-    public final static String MEMBER_MANAGEMENT_URL_PREFIX = "/management/member";
-    public final static String ADMIN_ACCOUNT_URL_PREFIX = "/admin/account";
+    public final static String ARTICLE_MANAGEMENT_URL_PREFIX = "/management/articles";
+    public final static String COMMENT_MANAGEMENT_URL_PREFIX = "/management/comments";
+    public final static String MEMBER_MANAGEMENT_URL_PREFIX = "/management/members";
+    public final static String ADMIN_ACCOUNT_URL_PREFIX = "/admin/accounts";
 }
