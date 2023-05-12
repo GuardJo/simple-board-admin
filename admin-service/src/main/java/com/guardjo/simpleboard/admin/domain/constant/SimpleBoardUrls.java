9540ad3fbb4b0f364bd82e3dc832d533e7a1fd6c
kyeongho <dagh1218@naver.com>
2023-05-12 23:54:50 +0900

    #15 게시글 데이터 수집 요청 및 응답 객체 구성
    - 게시글 데이터 요청 시 사용할 RestTemplate 객체 bean 구성
    - 응답받은 게시글 데이터 저장을 위한 Response 객체 구성
    -- simple-board api 명세에 따름
    - simple-board에서 제공해주는 api url 관리 객체 구성

---
+++ admin-service/src/main/java/com/guardjo/simpleboard/admin/domain/constant/SimpleBoardUrls.java
@@ -0,0 +1,6 @@
+package com.guardjo.simpleboard.admin.domain.constant;
+
+public class SimpleBoardUrls {
+    private final static String PREFIX_URL = "/api";
+    public final static String REQUEST_ARTICLES_URL = PREFIX_URL + "/articles";
+}
