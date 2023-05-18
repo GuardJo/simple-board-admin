4b522e35e7a354c1774e414d81c2fe55fa1326cd
kyeongho <dagh1218@naver.com>
2023-05-19 00:11:38 +0900

    #17 카카오 로그인 관련 설정 추가
    - oauth2Service 관련 bean 설정 추가
    -- 계정 정보가 없을 경우 신규 저장

--- admin-service/src/main/java/com/guardjo/simpleboard/admin/config/SecurityConfig.java
+++ admin-service/src/main/java/com/guardjo/simpleboard/admin/config/SecurityConfig.java
@@ -1,11 +1,23 @@
 package com.guardjo.simpleboard.admin.config;
 
+import com.guardjo.simpleboard.admin.model.AdminAccountDto;
+import com.guardjo.simpleboard.admin.model.security.BoardAdminPrincipal;
+import com.guardjo.simpleboard.admin.model.security.oauth2.kakao.KakaoOAuth2UserResponse;
+import com.guardjo.simpleboard.admin.service.AdminAccountService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -37,5 +49,41 @@ public class SecurityConfig {
                 .logout(logOutConfig -> logOutConfig.logoutSuccessUrl("/"));
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(AdminAccountService adminAccountService) {
+        return username ->
+                adminAccountService.searchAdminAccount(username)
+                        .map(BoardAdminPrincipal::from)
+                        .orElseThrow(() -> new UsernameNotFoundException(String.format("Not Found AdminAccount, email = %s", username)));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(AdminAccountService adminAccountService, PasswordEncoder passwordEncoder) {
+        DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
+
+        return userRequest -> {
+            OAuth2User oAuth2User = defaultOAuth2UserService.loadUser(userRequest);
+            KakaoOAuth2UserResponse kakaoOAuth2UserResponse = KakaoOAuth2UserResponse.from(oAuth2User.getAttributes());
+
+            AdminAccountDto adminAccountDto = adminAccountService.searchAdminAccount(kakaoOAuth2UserResponse.kakaoAccount().email())
+                    .orElseGet(() -> adminAccountService.createAdminAccount(
+                            kakaoOAuth2UserResponse.kakaoAccount().email(),
+                            kakaoOAuth2UserResponse.kakaoAccount().name(),
+                            passwordEncoder.encode(randomPassword(kakaoOAuth2UserResponse.id())))
+                    );
+
+            return BoardAdminPrincipal.from(adminAccountDto);
+        };
+    }
+
+    private String randomPassword(long kakaoRequestId) {
+        return "{bcryp}kakao_random_password" + String.valueOf(kakaoRequestId);
     }
 }
