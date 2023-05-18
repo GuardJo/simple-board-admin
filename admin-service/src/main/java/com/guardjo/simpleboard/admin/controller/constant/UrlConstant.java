ee202827ff356d7def7cba725e721f68b407e405
kyeongho <dagh1218@naver.com>
2023-05-18 23:50:57 +0900

    #15 관리자 계정 관련 비즈니스 코드 추가에 따른 컨틀로러 리팩토링
    - 관리자 계정 페이지 관련 컨트롤러 내 계정 데이터 반환 및 삭제 api 추가
    -- api 추가에 따른 신규 url 추가
    - controller 내 추가된 메소드들에 대한 신규 테스트 코드 생성

--- admin-service/src/main/java/com/guardjo/simpleboard/admin/controller/constant/UrlConstant.java
+++ admin-service/src/main/java/com/guardjo/simpleboard/admin/controller/constant/UrlConstant.java
@@ -5,4 +5,6 @@ public class UrlConstant {
     public final static String COMMENT_MANAGEMENT_URL_PREFIX = "/management/comments";
     public final static String MEMBER_MANAGEMENT_URL_PREFIX = "/management/members";
     public final static String ADMIN_ACCOUNT_URL_PREFIX = "/admin/accounts";
+
+    public final static String ADMIN_ACCOUNT_API_URL = "/api/admin/accounts";
 }
