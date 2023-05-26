49ba3624c079f6e1c5c8a32fac8c1679d138b5f9
kyeongho <dagh1218@naver.com>
2023-05-27 01:06:52 +0900

    #22 게시글 컨트롤러 내 서비스 연결
    - 게시글 관리 컨트롤러 내 관련 비즈니스 로직 연동
    - article 뷰에 model 데이터 삽입되도록 설정 추가

--- admin-service/src/main/java/com/guardjo/simpleboard/admin/controller/ArticleManagementController.java
+++ admin-service/src/main/java/com/guardjo/simpleboard/admin/controller/ArticleManagementController.java
@@ -1,18 +1,48 @@
 package com.guardjo.simpleboard.admin.controller;
 
 import com.guardjo.simpleboard.admin.controller.constant.UrlConstant;
+import com.guardjo.simpleboard.admin.model.ArticleDto;
+import com.guardjo.simpleboard.admin.service.ArticleManagementService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping(UrlConstant.ARTICLE_MANAGEMENT_URL_PREFIX)
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class ArticleManagementController {
+    private final ArticleManagementService articleManagementService;
+
     @GetMapping
-    public String getDefaultViewPage() {
-        log.info("[Test] Request Default Article Management Page");
+    public String getDefaultViewPage(Model model) {
+        log.info("Request Default Article Management Page");
+
+        List<ArticleDto> articleDtoList = articleManagementService.findArticles();
+
+        model.addAttribute("articles", articleDtoList);
+
         return "management/article";
+    }
+
+    @ResponseBody
+    @GetMapping("/{articleId}")
+    public ArticleDto getArticle(@PathVariable long articleId) {
+        log.info("Request Get Article, articleId = {}", articleId);
+
+        return articleManagementService.findArticle(articleId);
+    }
+
+    @DeleteMapping("/{articleId}")
+    public String deleteArticle(@PathVariable long articleId) {
+        log.info("Request Delete Article, articleId = {}", articleId);
+
+        articleManagementService.deleteArticle(articleId);
+
+        return "redirect:" + UrlConstant.ARTICLE_MANAGEMENT_URL_PREFIX;
     }
 }
