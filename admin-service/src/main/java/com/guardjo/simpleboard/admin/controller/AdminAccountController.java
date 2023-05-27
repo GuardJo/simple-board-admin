6b4b5ef86145e06f19a4e62f3606a6f125448ec5
kyeongho <dagh1218@naver.com>
2023-05-28 02:40:03 +0900

    #26 관리자 계정 관련 컨트롤러 내 서비스 연동
    - 구현된 실제 서비스 연동 및 일부 테스트 코드 개선

--- admin-service/src/main/java/com/guardjo/simpleboard/admin/controller/AdminAccountController.java
+++ admin-service/src/main/java/com/guardjo/simpleboard/admin/controller/AdminAccountController.java
@@ -2,6 +2,8 @@ package com.guardjo.simpleboard.admin.controller;
 
 import com.guardjo.simpleboard.admin.controller.constant.UrlConstant;
 import com.guardjo.simpleboard.admin.model.response.AdminAccountResponse;
+import com.guardjo.simpleboard.admin.service.AdminAccountService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -11,10 +13,13 @@ import java.util.List;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class AdminAccountController {
+    private final AdminAccountService adminAccountService;
+
     @GetMapping(UrlConstant.ADMIN_ACCOUNT_URL_PREFIX)
     public String getDefaultViewPage() {
-        log.info("[Test] Request Default Admin Account Page");
+        log.info("Request Default Admin Account Page");
         return "admin/account";
     }
 
@@ -22,12 +27,17 @@ public class AdminAccountController {
     @ResponseBody
     public List<AdminAccountResponse> getAdminAccountResponses() {
         log.info("Request Find AdminAccountResponses");
-        return List.of();
+
+        return adminAccountService.findAdminAccounts().stream()
+                .map(AdminAccountResponse::from)
+                .toList();
     }
 
     @DeleteMapping(UrlConstant.ADMIN_ACCOUNT_API_URL + "/{email}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteAdminAccount(@PathVariable String email) {
         log.info("Request Delete AdminAccountResponse, email = {}", email);
+
+        adminAccountService.deleteAdminAccount(email);;
     }
 }
